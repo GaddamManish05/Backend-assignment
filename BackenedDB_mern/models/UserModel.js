ef747c32e726d36184d87892eddf4d0e1cad3c63
GaddamManish05 <gaddam.mani1305@gmail.com>
2026-02-04 15:53:08 +0530

    Backend Assignments

---
+++ BackenedDB_mern/models/UserModel.js
@@ -0,0 +1,27 @@
+import {Schema,model} from 'mongoose'
+// create user schema
+//create user model  with that schema 
+
+const userSchema = new Schema({
+    username:{
+        type : String,
+        required : [true,"User Name is Required"],
+        minLenght : [4, "Minimum Length Must be 4"],
+        maxLength : [6, "Maximum Length Exceeded" ]
+    },
+    password : {
+        type : String,
+        required : [true, "Password is Required"]
+    },
+    age : {
+        type : Number,
+        required : [true,"Age is Required"],
+        min : [18, "Age Should be Above 18"],
+        max : [25, "Age IS Exceeded"]
+    }
+},{
+    strict : "throw", // which is used to validate the schema 
+    timestamps: true // which creates extra field of creating and updation type of field
+})
+// create and export the model
+export const UserModel = model("user",userSchema); 
